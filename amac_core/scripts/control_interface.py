78190f87ed7ca20ba0b5fc403a460c30ddefaa7a
Travis Brashears <trbrashears@berkeley.edu>
2018-12-08 03:24:23 -0800

    mods

--- amac_core/scripts/control_interface.py
+++ amac_core/scripts/control_interface.py
@@ -52,7 +52,12 @@ def car_control():
     pub_st_ang = rospy.Publisher('steering_angle', Float32, queue_size=10)
     rate = rospy.Rate(40) # 10hz
     while not rospy.is_shutdown():
-        velocity_pwm = velocity * 140 + 1475
+        if (velocity < 1.8):
+            gain = 140
+        else:
+            gain = 30
+
+        velocity_pwm = velocity * gain + 1475
         steering_pwm = steering_angle * 10 + 1450
 
 
